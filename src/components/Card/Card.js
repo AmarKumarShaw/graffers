18d0d28bd72377bef8054c4a760b4f351c294245
Amar Shaw <shawamar011@gmail.com>
2021-12-03 14:17:28 +0530

    Crads Ready Fetching yet to be made

---
+++ src/components/Card/Card.js
@@ -0,0 +1,124 @@
+import React from 'react'
+import "./Card.css"
+
+
+const Card = () => {
+    return (
+        <div className="cards">
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            <div className="card-content">
+                <div className="card-heading">
+                    <h1>Name:Amar Kumar Shaw</h1>
+                </div>
+                <div className="card-info">
+                    <p>Height:172</p>
+                    <p>Mass:77</p>
+                    <p>Hair Color:Blond</p>
+                    <p>Skin:Color</p>
+                    <p>Eye Color:Blue</p>
+
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Navbar.css";
+
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="navbar">
+            <div className="menu-icon">
+            <i class="fas fa-bars"></i>
+            </div>
+            </div>            
+        </>
+    )
+}
+
+export default Navbar
+ 
